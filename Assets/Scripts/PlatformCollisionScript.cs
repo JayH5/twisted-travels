5a62fa2a123b91bd1525908f748ec157e36cf39d
Matthew Wood <matthewwood4@gmail.com>
2014-05-19 22:59:07 +0200

    Very basic procedural generation

---
+++ Assets/Scripts/PlatformCollisionScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformCollisionScript : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+
+		//Destroy (gameObject.transform.parent.gameObject);
+		//Spawn ();
+		if (other.gameObject.tag != "Player")
+			print ("COLLISION PLATFORM!");
+	}
+}
