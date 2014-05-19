24bfc9646792c9142613f1ab566354acc510637a
Jamie Hewland <jhewland@gmail.com>
2014-05-19 23:15:43 +0200

    Add motion sickness.. um .. I mean camera rotation damping

--- Assets/External Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/External Assets/2D/Scripts/Camera2DFollow.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class Camera2DFollow : MonoBehaviour {
 	
 	public Transform target;
-	public float damping = 1;
+	public float positionDamping = 1;
 	public float lookAheadFactor = 3;
 	public float lookAheadReturnSpeed = 0.5f;
 	public float lookAheadMoveThreshold = 0.1f;
+
+	public float rotationDamping = 0.5f;
+	public float turnAheadFactor = 3;
+	public float turnAheadReturnSpeed = 10.0f;
+	public float turnAheadMoveThreshold = 0.1f;
 	
 	float offsetZ;
+
 	Vector3 lastTargetPosition;
 	Vector3 currentVelocity;
 	Vector3 lookAheadPos;
+
+	Vector3 lastTargetUp;
+	Vector3 currentAngularVelocity;
+	Vector3 turnAheadUp;
 	
 	// Use this for initialization
 	void Start () {
@@ -25,23 +35,49 @@ public class Camera2DFollow : MonoBehaviour {
 	void Update () {
 		
 		// only update lookahead pos if accelerating or changed direction
-		float xMoveDelta = (target.position - lastTargetPosition).x;
+		Vector3 movement = target.position - lastTargetPosition;
+		float moveDelta = Mathf.Abs(movement.x) > Mathf.Abs(movement.y) ? movement.x : movement.y;
 
-	    bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+	    bool updateLookAheadTarget = Mathf.Abs(moveDelta) > lookAheadMoveThreshold;
 
-		if (updateLookAheadTarget) {
-			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
-		} else {
+		if (updateLookAheadTarget)
+		{
+			lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(moveDelta);
+		}
+		else
+		{
 			lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);	
 		}
 		
 		Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
-		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+		Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, positionDamping);
 		
 		transform.position = newPos;
 		
 		lastTargetPosition = target.position;	
 
-		transform.up = Vector3.Normalize(target.up);
+
+		// Now do the same for rotation...
+		float rotDelta = Vector3.Angle(target.up, lastTargetUp);
+
+		bool updateTurnAheadTarget = Mathf.Abs(rotDelta) > turnAheadMoveThreshold;
+
+		if (updateTurnAheadTarget)
+		{
+			turnAheadUp = turnAheadFactor * Vector3.up * Mathf.Sign(rotDelta);
+		}
+		else
+		{
+			turnAheadUp = Vector3.MoveTowards(turnAheadUp, Vector3.zero, Time.deltaTime * turnAheadReturnSpeed);
+		}
+
+		Vector3 aheadTargetUp = target.up + turnAheadUp;
+		//Debug.Log ("Target up: " + target.up);
+		//Debug.Log ("aheadTargetUp: " + aheadTargetUp);
+		Vector3 newRot = Vector3.SmoothDamp(transform.up, aheadTargetUp, ref currentAngularVelocity, rotationDamping);
+
+		transform.up = newRot;
+
+		lastTargetUp = target.up;
 	}
 }
