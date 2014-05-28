f7337640fb88e54cdcf05f5a7d5bc7f52d5b5cce
Jamie Hewland <jhewland@gmail.com>
2014-05-28 16:34:47 +0200

    Cleanup

---
+++ Assets/Scripts/Triggers/BoxTriggerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Triggers {
+
+	public class BoxTriggerScript : MonoBehaviour {
+
+		void OnTriggerEnter2D(Collider2D other) 
+		{
+			if (other.tag == "Player")
+			{
+				print(transform.parent.gameObject.transform.rotation.z);
+				foreach (Transform child in transform)
+				{
+					if (transform.parent.gameObject.transform.rotation.z > 0)
+						child.rigidbody2D.gravityScale = -10;
+					else 
+						child.rigidbody2D.gravityScale = 10;
+				}
+			}
+
+				
+		}
+	}
+
+}
