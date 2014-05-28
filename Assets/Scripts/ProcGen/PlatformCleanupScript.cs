559cea10bfa746103f898ed6c228d748de4162bb
Matthew Wood <matthewwood4@gmail.com>
2014-05-28 21:23:45 +0200

    Added some prefabs, added BoxGravityDirectionScript

--- Assets/Scripts/ProcGen/PlatformCleanupScript.cs
+++ Assets/Scripts/ProcGen/PlatformCleanupScript.cs
@@ -6,6 +6,19 @@ public class PlatformCleanupScript: MonoBehaviour {
 	private float timer;
 	private bool playerTouch = false;
 
+	void Start()
+	{
+		/*foreach (Transform child in transform)
+		{
+			if (gameObject.transform.rotation.z > 0 && child.gameObject.name == "Box")
+				child.rigidbody2D.gravityScale = -8;
+			else if (child.gameObject.name == "Box")
+				child.rigidbody2D.gravityScale = 8;
+		}*/
+
+		print (transform.rotation.z);
+	}
+
 	void Update ()
 	{
 		if (playerTouch)
